10e25915fdc9c19822ae80892fbad1a660c300c1
Seelen Maurits - A1800227 <maurits.seelen@edu.lapinamk.fi>
2018-04-27 15:03:59 +0300

    optimized de zoekfunctie

--- portfolio-italent/app/controllers/seminars.js
+++ portfolio-italent/app/controllers/seminars.js
@@ -4,9 +4,13 @@ export default Controller.extend({
     actions: {
         filterByName(param) {
             if (param !== '') {
-                return this.get('store').query('seminar', { name: param });
+                return this.get('store').query('seminar', { name: param }).then((results) => {
+                    return { query:param, results: results };
+                });
             } else {
-                return this.get('store').findAll('seminar');
+                return this.get('store').findAll('seminar').then((results) => {
+                    return { query:param, results: results };
+                })
             }
         }
     }
